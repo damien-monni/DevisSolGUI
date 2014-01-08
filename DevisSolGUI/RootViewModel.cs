e2bd393199e140862963525cf813547281bfd0d4
damien <monni.damien@gmail.com>
2014-01-08 10:48:52 +0100

    Add Menu and RootViewModel class

---
+++ DevisSolGUI/RootViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace DevisSolGUI
+{
+    class RootViewModel
+    {
+        public ObservableCollection<Object> ViewCollection { get; set; }
+
+
+        //Constructeur
+        public RootViewModel()
+        {
+            ViewCollection = new ObservableCollection<object>();
+            ViewCollection.Add(new Menu());
+
+        }
+    }
+}
